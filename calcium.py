a6400f97122340b21c7258007ac1933222b02c81
Disequilibrium <disequilibrium4772@protonmail.com>
2022-12-02 06:21:58 -0500

    First commit

---
+++ calcium.py
@@ -0,0 +1,61 @@
+import discord
+from discord import FFmpegPCMAudio
+from discord.ext import commands, tasks
+from discord.utils import get
+from youtube_dl import YoutubeDL
+import requests
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command('help')
+
+TOKEN = ''
+
+def search(query):
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist': 'True'}) as ydl:
+        try: requests.get(query)
+        except: info = ydl.extract_info(f"ytsearch:{query}", download = False)['entries'][0]
+        else: info = ydl.extract_info(query, download = False)
+    return (info, info['formats'][0]['url'])
+
+async def join(ctx, voice):
+    channel = ctx.author.voice.channel
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+
+@bot.command(pass_context=True)
+async def play(ctx, *, query):
+    print('yolo')
+    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    video, source = search(query)
+    voice = get(bot.voice_clients, guild = ctx.guild)
+
+    await join(ctx, voice)
+    #await ctx.send(f"Now playing {info['title']}")
+    voice = get(bot.voice_clients, guild = ctx.guild)
+
+    if voice.is_playing():
+        voice.stop()
+    voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS, executable="D:/ffmpeg/bin/ffmpeg.exe"), after=lambda e: print('done', e))
+
+@bot.command(pass_context=True)
+async def stop(ctx):
+    voice = get(bot.voice_clients, guild = ctx.guild)
+    voice.stop()
+
+@bot.command(pass_context=True)
+async def disconnect(ctx):
+    voice = get(bot.voice_clients, guild = ctx.guild)
+    await voice.disconnect(force = True)
+
+@bot.event
+async def on_ready():
+    print('Bot initialized')
+    await bot.change_presence(activity=discord.Game('!play !stop !disconnect'))
+
+bot.run()
